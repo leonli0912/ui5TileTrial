284953c2e088f81785cc2134cabf944c87af8d77
Leon Li <leon.li02@sap.com>
2016-03-30 18:04:47 +0800

    try ajax

--- WebContent/controller/Stock.controller.js
+++ WebContent/controller/Stock.controller.js
@@ -1,20 +1,49 @@
-sap.ui.define([ 'jquery.sap.global', 'sap/m/MessageToast',
-		'sap/ui/core/Fragment', 'sap/ui/core/mvc/Controller' ], function(
-		jQuery, MessageToast, Fragment, Controller) {
-	"use strict";
+sap.ui
+		.define(
+				[ 'jquery.sap.global', 'sap/m/MessageToast',
+						'sap/ui/core/Fragment', 'sap/ui/core/mvc/Controller' ],
+				function(jQuery, MessageToast, Fragment, Controller) {
+					"use strict";
 
-	var CController = Controller.extend("ui5TileTrial.controller.Stock", {
-		onInit:function(){
-			
-			var stockModel = new sap.ui.model.json.JSONModel();
-			//stockModel.loadData("https://s12hanaxs.hanatrial.ondemand.com/i074178trial/stock/test/test.xsjs");
-			stockModel.loadData("model/stock.json");
-			this.getView().setModel(stockModel);
-			var oTable=this.byId("idStockHistory");
-			oTable.setModel(stockModel);
+					var CController = Controller
+							.extend(
+									"ui5TileTrial.controller.Stock",
+									{
+										onInit : function() {
 
-			oTable.placeAt("content");
-		}
-		
-	});
-})
+											var stockModel = new sap.ui.model.json.JSONModel();
+											// stockModel.loadData("https://s12hanaxs.hanatrial.ondemand.com/i074178trial/stock/test/test.xsjs");
+											// stockModel.loadData("model/stock.json");
+											/*$
+													.ajax(
+															{
+																type : "POST",
+																dataType : 'jsonp',
+																url : "https://s12hanaxs.hanatrial.ondemand.com/i074178trial/stock/test/test.xsjs",
+																username : 'i074178',
+																password : 'Initial9',
+																crossDomain : true,
+																xhrFields : {
+																	withCredentials : true
+																}
+															})
+													.done(function(data) {
+														console.log("done");
+													})
+													.fail(
+															function(xhr,
+																	textStatus,
+																	errorThrown) {
+																alert(xhr.responseText);
+																alert(textStatus);
+															});*/
+											this.getView().setModel(stockModel);
+											var oTable = this
+													.byId("idStockHistory");
+											oTable.setModel(stockModel);
+
+											oTable.placeAt("content");
+										}
+
+									});
+				})
