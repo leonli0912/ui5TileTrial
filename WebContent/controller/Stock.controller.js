313b1592d053b8ee1f5e33452b07f2026040f60b
mac <mac@192.168.2.101>
2016-04-10 00:21:38 +0800

    set table model with json model from real odata
    Change-Id: I197f300e23e61cb9ec142ea746176ca061a38060

--- WebContent/controller/Stock.controller.js
+++ WebContent/controller/Stock.controller.js
@@ -10,38 +10,41 @@ sap.ui
 									"ui5TileTrial.controller.Stock",
 									{
 										onInit : function() {
-
+											var oDataServerUrl = "http://120.27.144.171:8080/Odata/Cloud_Hr.svc/";
 											var stockModel = new sap.ui.model.json.JSONModel();
 											// stockModel.loadData("https://s12hanaxs.hanatrial.ondemand.com/i074178trial/stock/test/test.xsjs");
-											stockModel.loadData("model/stock.json");
-											$
-													.ajax(
+											//http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=AAPL&callback=?
+											//stockModel.loadData("model/stock.json");
+											var that = this;
+											$.ajax(
 															{
-																type : "POST",
-																dataType : 'jsonp',
-																url : "http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=AAPL&callback=?",
-															    jsonpCallback: 'processJson',
+																type : "GET",
+																dataType : 'json',
+																url : "http://120.27.144.171:8080/Odata/Cloud_Hr.svc/Customers?$format=json",
+															    //jsonpCallback: 'processJson',
 															    contentType: "application/json",
-															    dataType: 'jsonp',
-															    success: function(json) {
+															   // dataType: 'jsonp',
+															    success: function(json,that) {
+															       
 															       console.log("success...");
+															       var stockModel = new sap.ui.model.json.JSONModel();
+															       stockModel.setData(json.d.results);
+															       _setTableModel(stockModel);
+															      /* that.getView().setModel(stockModel);
+															       var stockModel = that.getView().getModel();
+															       var oTable = that.byId("id_stockList");
+															       oTable.setModel(stockModel);*/
 															    },
 															    error: function(e) {
 															       console.log(e.message);
 															    }
 															});
-											window.processJson = function(data){
-												
-											console.log("processing data...");
+											var _setTableModel =function(oModel){
+												that.getView().setModel(oModel);
+											    var oTable = that.byId("id_stockList");
+											    oTable.setModel(oModel);
 											};
-													
-											
-											this.getView().setModel(stockModel);
-											var oTable = this
-													.byId("id_stockList");
-											oTable.setModel(stockModel);
-
-											
+										
 										},
 										onPressHome:function(){
 											var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
