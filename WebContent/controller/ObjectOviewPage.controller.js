c9972379af098fdc8c00161a155f34ba1edd9aa6
Leon Li <leon.li02@sap.com>
2016-04-14 16:03:22 +0800

    success to show data in blocks
    Change-Id: Idee17e597275f664a62c66698b99467220f396fa

--- WebContent/controller/ObjectOviewPage.controller.js
+++ WebContent/controller/ObjectOviewPage.controller.js
@@ -1,8 +1,8 @@
 sap.ui.controller("ui5TileTrial.controller.ObjectOviewPage",{
 	onInit:function(){
 		var oJsonModel = new sap.ui.model.json.JSONModel();
-		oJsonModel.loadData("model/Cont_Info_sample.json");
-		sap.ui.getCore().setModel(oJsonModel, "Cont_info");
+		oJsonModel.loadData("model/Cont_Info_sample.json","",false);
+		this.getView().setModel(oJsonModel, "Cont_info");
 	},
 	handleHomePress:function(oEvt){
 		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
