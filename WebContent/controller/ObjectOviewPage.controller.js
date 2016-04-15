1c585e0bb104dea0ec4702f19471edd7e87a1837
Leon Li <leon.li02@sap.com>
2016-04-15 17:21:08 +0800

    Prepare models
    Change-Id: Ic11e2ea7cc0e8b63754ded777f6f84837c6d9cf4

--- WebContent/controller/ObjectOviewPage.controller.js
+++ WebContent/controller/ObjectOviewPage.controller.js
@@ -1,9 +1,40 @@
 sap.ui.controller("ui5TileTrial.controller.ObjectOviewPage", {
 	SERVICE_URL : "http://120.27.144.171:8080/Odata/Cloud_Hr.svc/",
 	onInit : function() {
+		//init model
+		this._initModel("Cont_Infos");
+		this._initModel("Employee_Infos");
+		this._initModel("Employment_Infos");
+		
+/*		OData.read({
+			requestUri : this.SERVICE_URL+"Cont_Infos",
+			headers : {
+				Accept : "application/json"
+			}
+		}, function(data, response) {
+			
+			var oJsonModel = new sap.ui.model.json.JSONModel();
+			oJsonModel.setData(data);
+			_setTableModel(oJsonModel,that);
+		}, function(err) {
+			alert("Error occurred " + err.message);
+		});
+
+		var oJsonModel = new sap.ui.model.json.JSONModel();
+		oJsonModel.loadData("model/Cont_Info_sample.json", "", false);
+		this.getView().setModel(oJsonModel, "Cont_info");
+		var _setTableModel = function(oModel){
+			that.getView().setModel(oModel, "Cont_info");
+		}*/
+	},
+	handleHomePress : function(oEvt) {
+		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+		oRouter.navTo("appHome");
+	},
+	_initModel: function(oEntitySet){
 		var that = this;
 		OData.read({
-			requestUri : this.SERVICE_URL+"Cont_Infos",
+			requestUri : this.SERVICE_URL+oEntitySet,
 			headers : {
 				Accept : "application/json"
 			}
@@ -20,11 +51,7 @@ sap.ui.controller("ui5TileTrial.controller.ObjectOviewPage", {
 		oJsonModel.loadData("model/Cont_Info_sample.json", "", false);
 		this.getView().setModel(oJsonModel, "Cont_info");*/
 		var _setTableModel = function(oModel){
-			that.getView().setModel(oModel, "Cont_info");
+			that.getView().setModel(oModel, oEntitySet);
 		}
-	},
-	handleHomePress : function(oEvt) {
-		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-		oRouter.navTo("appHome");
 	}
 })
