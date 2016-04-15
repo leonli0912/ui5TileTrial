98178eea6595c0c81fdbd9209e341dc6718fa05e
Leon Li <leon.li02@sap.com>
2016-04-15 17:08:32 +0800

    access real odata
    Change-Id: I9cdb0aed3f51a43da12fa2ac677bcac27138e77e

--- WebContent/controller/ObjectOviewPage.controller.js
+++ WebContent/controller/ObjectOviewPage.controller.js
@@ -1,10 +1,29 @@
-sap.ui.controller("ui5TileTrial.controller.ObjectOviewPage",{
-	onInit:function(){
-		var oJsonModel = new sap.ui.model.json.JSONModel();
-		oJsonModel.loadData("model/Cont_Info_sample.json","",false);
-		this.getView().setModel(oJsonModel, "Cont_info");
+sap.ui.controller("ui5TileTrial.controller.ObjectOviewPage", {
+	SERVICE_URL : "http://120.27.144.171:8080/Odata/Cloud_Hr.svc/",
+	onInit : function() {
+		var that = this;
+		OData.read({
+			requestUri : this.SERVICE_URL+"Cont_Infos",
+			headers : {
+				Accept : "application/json"
+			}
+		}, function(data, response) {
+			
+			var oJsonModel = new sap.ui.model.json.JSONModel();
+			oJsonModel.setData(data);
+			_setTableModel(oJsonModel,that);
+		}, function(err) {
+			alert("Error occurred " + err.message);
+		});
+
+/*		var oJsonModel = new sap.ui.model.json.JSONModel();
+		oJsonModel.loadData("model/Cont_Info_sample.json", "", false);
+		this.getView().setModel(oJsonModel, "Cont_info");*/
+		var _setTableModel = function(oModel){
+			that.getView().setModel(oModel, "Cont_info");
+		}
 	},
-	handleHomePress:function(oEvt){
+	handleHomePress : function(oEvt) {
 		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 		oRouter.navTo("appHome");
 	}
