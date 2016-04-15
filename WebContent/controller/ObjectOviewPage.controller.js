9a5071b5b4b31c24f39751c711d477683b1de890
mac <mac@192.168.2.101>
2016-04-16 02:40:45 +0800

    try edit page
    Change-Id: I2989f8426ca0e50c898dbac3514704d1775f2450

--- WebContent/controller/ObjectOviewPage.controller.js
+++ WebContent/controller/ObjectOviewPage.controller.js
@@ -1,35 +1,19 @@
 sap.ui.controller("ui5TileTrial.controller.ObjectOviewPage", {
 	SERVICE_URL : "http://120.27.144.171:8080/Odata/Cloud_Hr.svc/",
+	_formFragments: {},
 	onInit : function() {
 		//init model
 		this._initModel("Cont_Infos");
 		this._initModel("Employee_Infos");
 		this._initModel("Employment_Infos");
 		
-/*		OData.read({
-			requestUri : this.SERVICE_URL+"Cont_Infos",
-			headers : {
-				Accept : "application/json"
-			}
-		}, function(data, response) {
-			
-			var oJsonModel = new sap.ui.model.json.JSONModel();
-			oJsonModel.setData(data);
-			_setTableModel(oJsonModel,that);
-		}, function(err) {
-			alert("Error occurred " + err.message);
-		});
-
-		var oJsonModel = new sap.ui.model.json.JSONModel();
-		oJsonModel.loadData("model/Cont_Info_sample.json", "", false);
-		this.getView().setModel(oJsonModel, "Cont_info");
-		var _setTableModel = function(oModel){
-			that.getView().setModel(oModel, "Cont_info");
-		}*/
 	},
 	handleHomePress : function(oEvt) {
 		var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 		oRouter.navTo("appHome");
+	},
+	onEditPage:function(){
+		this._toggleButtonsAndView(true);
 	},
 	_initModel: function(oEntitySet){
 		var that = this;
@@ -53,5 +37,33 @@ sap.ui.controller("ui5TileTrial.controller.ObjectOviewPage", {
 		var _setTableModel = function(oModel){
 			that.getView().setModel(oModel, oEntitySet);
 		}
-	}
+	},
+	_toggleButtonsAndView:function(bEdit){
+		var oView = this.getView();
+		 
+		// Show the appropriate action buttons
+		oView.byId("id_edit").setVisible(!bEdit);
+		oView.byId("id_save").setVisible(bEdit);
+		oView.byId("id_undo").setVisible(bEdit);
+
+		// Set the right form type
+		this._showFormFragment(bEdit ? "Change" : "Display");
+	},
+	_showFormFragment : function (sFragmentName) {
+		var oPage = this.getView().byId("page");
+
+		oPage.removeAllContent();
+		oPage.insertContent(this._getFormFragment(sFragmentName));
+	},
+	_getFormFragment: function (sFragmentName) {
+		var oFormFragment = this._formFragments[sFragmentName];
+
+		if (oFormFragment) {
+			return oFormFragment;
+		}
+
+		oFormFragment = sap.ui.xmlfragment(this.getView().getId(), "sap.ui.layout.sample.SimpleForm354." + sFragmentName);
+
+		return this._formFragments[sFragmentName] = oFormFragment;
+	},
 })
